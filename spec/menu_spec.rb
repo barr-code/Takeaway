166b2041a43cfd8c03c0b08501c15e70fafeca26
Victoria Barr <vb329@georgetown.edu>
2014-09-27 23:37:39 +0100

    Created Menu Class

---
+++ spec/menu_spec.rb
@@ -0,0 +1,16 @@
+require 'menu.rb'
+
+describe Menu do 
+
+	let(:menu) {Menu.new}
+	let(:dish) {double :dish}
+
+	it "should have an array of dishes" do
+		expect(menu.dishes).to eq([])
+	end
+
+	it "should be able to add new dishes" do
+		menu.add_dish(dish)
+		expect(menu).to have_dishes
+	end
+end
