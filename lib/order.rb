68dea1eaacc8ddd8c2f9913fcf2615ada2bb3d1f
Victoria Barr <vb329@georgetown.edu>
2014-09-28 15:34:50 +0100

    Order generates total price

--- lib/order.rb
+++ lib/order.rb
@@ -1,8 +1,20 @@
 class Order
 
-	attr_accessor :line_items
+	attr_accessor :line_items, :order_total
 
 	def initialize
 		@line_items = []
+		@order_total = 0
+	end
+
+	def add_line_item(line_item)
+		@line_items << line_item
+	end
+
+	def grand_total
+		@line_items.each do |item|
+			@order_total += item.line_total
+		end
+		@order_total
 	end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,9 +3,22 @@ require 'order'
 describe Order do 
 
 	let(:order) {Order.new}
+	let(:line_item) {double :line_item}
 
 	it "should have a list of line items" do
 		expect(order.line_items.class).to eq(Array)
+	end
+
+	it "should be able to add new line items to the list" do
+		expect{order.add_line_item(line_item)}.to change{order.line_items.count}.by(1)
+	end
+
+	it "should generate a total price for the order" do
+		line1 = double :line_item, :line_total => 10
+		line2 = double :line_item, :line_total => 8
+		order.add_line_item(line1)
+		order.add_line_item(line2)
+		expect(order.grand_total).to eq(18)
 	end
 
 end
